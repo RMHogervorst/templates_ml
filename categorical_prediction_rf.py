f8f0a47ab22228e0bcb373f61c8f25bf617c1d60
Roel M. Hogervorst <hogervorst.rm@gmail.com>
2022-08-01 16:44:39 +0200

    more complete example

--- categorical_prediction_rf.py
+++ categorical_prediction_rf.py
@@ -8,8 +8,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
+from sklearn.datasets import make_classification
+from sklearn.metrics import accuracy_score, cohen_kappa_score
 
 ## Split file into training and test set ----
+X, y = make_classification(
+    n_samples=100_000, n_features=20, n_informative=2, n_redundant=2, random_state=42
+)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.33, random_state=42
 )
@@ -25,4 +30,7 @@ clf = Pipeline([("median_imputer", median_imputer), ("classification", classifie
 ## fit on trainingset
 clf.fit(X_train, y_train)
 ## evaluate on testset
-clf.predict(X_test)
+clf.score(X_test, y_test)
+y_pred = clf.predict(X_test)
+accuracy_score(y_test, y_pred)
+cohen_kappa_score(y_test, y_pred)
